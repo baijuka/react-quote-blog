ecd849a2c6842adc5aa2defb5e9a2ed59d36f16e
Baiju Abraham <baijuka@yahoo.com>
2022-09-11 00:47:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Pagination from "./components/Pagination";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Pagination />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+import axios from "axios";
+
+const renderData = (data) => {
+  return (
+    <ul>
+      {data.map((todo, index) => {
+        return (<div className="quoteClass"><li key={index}>{todo.text}</li> <li>- {todo.author}</li></div>);
+      })}
+    </ul>
+  );
+};
+
+function Pagination() {
+  const [data, setData] = useState([]);
+
+  const [currentPage, setcurrentPage] = useState(1);
+  const [itemsPerPage] = useState(5);
+
+  const [pageNumberLimit] = useState(5);
+  const [maxPageNumberLimit, setmaxPageNumberLimit] = useState(5);
+  const [minPageNumberLimit, setminPageNumberLimit] = useState(0);
+
+  const handleClick = (event) => {
+    setcurrentPage(Number(event.target.id));
+  };
+
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
+    pages.push(i);
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const renderPageNumbers = pages.map((number) => {
+    if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
+      return (
+        <li
+          key={number}
+          id={number}
+          onClick={handleClick}
+          className={currentPage === number ? "active" : null}
+        >
+          {number}
+        </li>
+      );
+    } else {
+      return null;
+    }
+  });
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await axios.get('https://type.fit/api/quotes');
+      setData(response.data);
+    }
+    fetchPosts();
+  },[])
+
+  const handleNextbtn = () => {
+    setcurrentPage(currentPage + 1);
+
+    if (currentPage + 1 > maxPageNumberLimit) {
+      setmaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit + pageNumberLimit);
+    }
+  };
+
+  const handlePrevbtn = () => {
+    setcurrentPage(currentPage - 1);
+
+    if ((currentPage - 1) % pageNumberLimit === 0) {
+      setmaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
+      setminPageNumberLimit(minPageNumberLimit - pageNumberLimit);
+    }
+  };
+
+  let pageIncrementBtn = null;
+  if (pages.length > maxPageNumberLimit) {
+    pageIncrementBtn = <li onClick={handleNextbtn}> &hellip; </li>;
+  }
+
+  let pageDecrementBtn = null;
+  if (minPageNumberLimit >= 1) {
+    pageDecrementBtn = <li onClick={handlePrevbtn}> &hellip; </li>;
+  }
+
+ 
+  return (
+    <>
+      <h1>Quote Shower</h1> <br />
+      {renderData(currentItems)}
+      <ul className="pageNumbers">
+        <li>
+          <button
+            onClick={handlePrevbtn}
+            disabled={currentPage === pages[0] ? true : false}
+          >
+            Prev
+          </button>
+        </li>
+        {pageDecrementBtn}
+        {renderPageNumbers}
+        {pageIncrementBtn}
+
+        <li>
+          <button
+            onClick={handleNextbtn}
+            disabled={currentPage === pages[pages.length - 1] ? true : false}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Pagination;
